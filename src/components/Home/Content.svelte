41728eb2c8792fbc56bd24a337ed74898c823c31
Marcel099 <44550398+Marcel099@users.noreply.github.com>
2022-04-05 00:47:15 -0300

    Style for auto-generating widths of books

--- src/components/Home/Content.svelte
+++ src/components/Home/Content.svelte
@@ -1,1 +1,9 @@
 <main class="home-content"></main>
+
+<style>
+  .home-content {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(150px, 1fr));
+    width: 100%;
+  }
+</style>
