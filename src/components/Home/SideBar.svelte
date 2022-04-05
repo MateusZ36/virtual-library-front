8c85366bd7a1cd184b5f4e0fa3addc08d5fbeb52
Marcel099 <44550398+Marcel099@users.noreply.github.com>
2022-04-05 00:12:59 -0300

    Added main containers for the 2 pages

---
+++ src/components/Home/SideBar.svelte
@@ -0,0 +1,10 @@
+<aside class="home-sidebar"></aside>
+
+
+<style>
+  .home-sidebar {
+    min-width: 15rem;
+    max-width: 35%;
+    width: 100%;
+  }
+</style>
--- src/pages/BorrowBooks.svelte
+++ src/pages/BorrowBooks.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
   import { Link } from "svelte-navigator";
+  import BorrowBooksContainer from "../components/BorrowBooks/BorrowBooksContainer.svelte";
 </script>
 
 <h1>Cesta de livros</h1>
 
 <Link to="/">Home</Link>
+
+<BorrowBooksContainer />
