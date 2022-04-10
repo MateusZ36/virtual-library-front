65e689910f5dab5b18601ea6ab547c781fcec5c1
Marcel099 <44550398+Marcel099@users.noreply.github.com>
2022-04-10 18:42:00 -0300

    Renaming file index.svelte to content.svelte

---
+++ src/components/Home/Content.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  
+  import { api } from "../../services/api";
+  import { shelfStore } from "../../stores/shelf";
+  import BookShelf from "./Content/BookShelf.svelte";
+
+  let shelf = [];
+
+  shelfStore.subscribe(newValue => shelf = newValue)
+
+  onMount(async () => {
+    try {
+      const response = await api.get('/books');
+      shelfStore.update(() => response.data)
+    } catch(err) {
+      console.error(err);
+    }
+  })
+</script>
+
+<main class="home-content">
+  {#each shelf as item}
+    <BookShelf book={item} />
+  {/each}
+</main>
+
+<style>
+  .home-content {
+    display: flex;
+    flex-wrap: wrap;
+  }
+</style>
